5209548cbc68c76002c2b284843b32f78208d804
Jonathan <jlgroves91@gmail.com>
2023-09-19 22:24:22 -0400

    adjust content

--- src/js/main.js
+++ src/js/main.js
@@ -51,5 +51,20 @@ jQuery( document ).ready(function($) {
       $('.bit-button').html('More Info')
       $('.bit-button').attr('href', $('.bit-button').attr('href').split('?')[0]) 
     }, 50)
+
+    function debounce(func){
+        var timer;
+        return function(event){
+          if(timer) clearTimeout(timer);
+          timer = setTimeout(func,100,event);
+        };
+      }
+
+      window.addEventListener("resize",debounce(function(e){
+        setTimeout(function(){
+            $('.bit-button').html('More Info')
+            $('.bit-button').attr('href', $('.bit-button').attr('href').split('?')[0]) 
+          }, 50)
+      }));
    
 });
